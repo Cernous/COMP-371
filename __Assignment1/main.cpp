fb061657cfa37185257604d9e5a59e07ffb44213
Cernous <redtraitor9@gmail.com>
2025-07-24 15:34:23 -0400

    assignment fullscreen

--- __Assignment1/main.cpp
+++ __Assignment1/main.cpp
@@ -1,26 +1,22 @@
-/*
-    Assignment 1 Project
-    for COMP 371
-
-    40166293
-    40263250
-
-*/
+//
+// COMP 371 Labs Framework
+//
+// Created by Nicolas Bergeron on 20/06/2019.
+//
 
 #include <iostream>
+#include <list>
 
 #define GLEW_STATIC 1   // This allows linking with Static Library on Windows, without DLL
-
 #include <GL/glew.h>    // Include GLEW - OpenGL Extension Wrangler
 
-#include <GLFW/glfw3.h> // GLFW provides a cross-platform interface for creating a graphical context,
+#include <GLFW/glfw3.h> // cross-platform interface for creating a graphical context,
                         // initializing OpenGL and binding inputs
 
 #include <glm/glm.hpp>  // GLM is an optimized math library with syntax to similar to OpenGL Shading Language
-
 #include <glm/gtc/matrix_transform.hpp> // include this to create transformation matrices
-
 #include <glm/common.hpp>
+
 
 using namespace glm;
 using namespace std;
@@ -34,17 +30,18 @@ const char* getVertexShaderSource()
                 "layout (location = 1) in vec3 aColor;"
                 ""
                 "uniform mat4 worldMatrix;"
-                "uniform mat4 viewMatrix = mat4(1.0f);"
-                "uniform mat4 projectionMatrix = mat4(1.0f);"
+                "uniform mat4 viewMatrix = mat4(1.0);"  // default value for view matrix (identity)
+                "uniform mat4 projectionMatrix = mat4(1.0);"
                 ""
                 "out vec3 vertexColor;"
                 "void main()"
                 "{"
                 "   vertexColor = aColor;"
                 "   mat4 modelViewProjection = projectionMatrix * viewMatrix * worldMatrix;"
-                "   gl_Position =  modelViewProjection * vec4(aPos.x, aPos.y, aPos.z, 1.0);"
+                "   gl_Position = modelViewProjection * vec4(aPos.x, aPos.y, aPos.z, 1.0);"
                 "}";
 }
+
 
 const char* getFragmentShaderSource()
 {
@@ -57,6 +54,7 @@ const char* getFragmentShaderSource()
                 "   FragColor = vec4(vertexColor.r, vertexColor.g, vertexColor.b, 1.0f);"
                 "}";
 }
+
 
 int compileAndLinkShaders()
 {
@@ -116,8 +114,8 @@ int compileAndLinkShaders()
 
 int createVertexBufferObject()
 {
-        // Cube model
-        vec3 vertexArray[] = {  // position,                            color
+    // Cube model
+    vec3 vertexArray[] = {  // position,                            color
         vec3(-0.5f,-0.5f,-0.5f), vec3(1.0f, 0.0f, 0.0f), //left - red
         vec3(-0.5f,-0.5f, 0.5f), vec3(1.0f, 0.0f, 0.0f),
         vec3(-0.5f, 0.5f, 0.5f), vec3(1.0f, 0.0f, 0.0f),
@@ -198,27 +196,27 @@ int createVertexBufferObject()
                           (void*)sizeof(vec3)      // color is offseted a vec3 (comes after position)
                           );
     glEnableVertexAttribArray(1);
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindVertexArray(0);
 
     
     return vertexBufferObject;
 }
 
 
-// Main function
 int main(int argc, char*argv[])
 {
-    // Initialize GLFW and openGL
+    // Initialize GLFW and OpenGL version
     glfwInit();
-
+    
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
     // Create Window and rendering context using GLFW, resolution is 800x600
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Comp371 - Lab 03", NULL, NULL);
+    int mWidth, mHeight = 0;
+    GLFWmonitor* monitor = glfwGetPrimaryMonitor();
+    glfwGetMonitorPhysicalSize(monitor, &mWidth, &mHeight);
+    GLFWwindow* window = glfwCreateWindow(mWidth, mHeight, "Comp371 - Lab 03", monitor, NULL);
     if (window == NULL)
     {
         std::cerr << "Failed to create GLFW window" << std::endl;
@@ -227,8 +225,10 @@ int main(int argc, char*argv[])
     }
     glfwMakeContextCurrent(window);
 
+    // @TODO 3 - Disable mouse cursor
+    // ...
     glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-
+    
     // Initialize GLEW
     glewExperimental = true; // Needed for core profile
     if (glewInit() != GLEW_OK) {
@@ -237,26 +237,33 @@ int main(int argc, char*argv[])
         return -1;
     }
 
-    // Background color (black)
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    // Very important for different scaling and screen size ratio
+    int width, height;
+    glfwGetFramebufferSize(window, &width, &height);
+    glViewport(0, 0, width, height);
 
+    // Black background
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    
     // Compile and link shaders here ...
     int shaderProgram = compileAndLinkShaders();
-
+    
     // We can set the shader once, since we have only one
     glUseProgram(shaderProgram);
 
+    
     // Camera parameters for view transform
-    vec3 cameraPosition(1.0f,1.0f,10.0f);
+    vec3 cameraPosition(0.6f,1.0f,10.0f);
     vec3 cameraLookAt(0.0f, 0.0f, -1.0f);
     vec3 cameraUp(0.0f, 1.0f, 0.0f);
-
-     // Other camera parameters
+    
+    // Other camera parameters
     float cameraSpeed = 1.0f;
     float cameraFastSpeed = 2 * cameraSpeed;
     float cameraHorizontalAngle = 90.0f;
     float cameraVerticalAngle = 0.0f;
-
+    bool  cameraFirstPerson = true; // press 1 or 2 to toggle this variable
+    
     // Set projection matrix for shader, this won't change
     mat4 projectionMatrix = glm::perspective(70.0f,            // field of view in degrees
                                              800.0f / 600.0f,  // aspect ratio
@@ -264,29 +271,34 @@ int main(int argc, char*argv[])
     
     GLuint projectionMatrixLocation = glGetUniformLocation(shaderProgram, "projectionMatrix");
     glUniformMatrix4fv(projectionMatrixLocation, 1, GL_FALSE, &projectionMatrix[0][0]);
-    
+
+    // Set initial view matrix
     mat4 viewMatrix = lookAt(cameraPosition,  // eye
                              cameraPosition + cameraLookAt,  // center
                              cameraUp ); // up
-
+    
     GLuint viewMatrixLocation = glGetUniformLocation(shaderProgram, "viewMatrix");
     glUniformMatrix4fv(viewMatrixLocation, 1, GL_FALSE, &viewMatrix[0][0]);
 
+    
+    
     // Define and upload geometry to the GPU here ...
     int vao = createVertexBufferObject();
-
+    
     // For frame time
     float lastFrameTime = glfwGetTime();
     int lastMouseLeftState = GLFW_RELEASE;
     double lastMousePosX, lastMousePosY;
     glfwGetCursorPos(window, &lastMousePosX, &lastMousePosY);
-
+    
+    // Other OpenGL states to set once
     // Enable Backface culling
-    //glEnable(GL_CULL_FACE);
-
-    // Depth test
+    // glEnable(GL_CULL_FACE);
+    
+    // @TODO 1 - Enable Depth Test
+    // ...
     glEnable(GL_DEPTH_TEST);
-
+    
     // Entering Main Loop
     while(!glfwWindowShouldClose(window))
     {
@@ -294,10 +306,13 @@ int main(int argc, char*argv[])
         float dt = glfwGetTime() - lastFrameTime;
         lastFrameTime += dt;
 
-        // clears depth buffer bit.
+        // Each frame, reset color of each pixel to glClearColor
+
+        // @TODO 1 - Clear Depth Buffer Bit as well
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
         
-        // Draw Geometry:
+        // Draw geometry
         glBindVertexArray(vao);
 
         // Draw ground
@@ -306,71 +321,128 @@ int main(int argc, char*argv[])
         glUniformMatrix4fv(worldMatrixLocation, 1, GL_FALSE, &groundWorldMatrix[0][0]);
         
         glDrawArrays(GL_TRIANGLES, 0, 36); // 36 vertices, starting at index 0
+        
+        // Draw pillars
+        mat4 pillarWorldMatrix = translate(mat4(1.0f), vec3(0.0f, 10.0f, 0.0f)) * scale(mat4(1.0f), vec3(2.0f, 20.0f, 2.0f));
+        glUniformMatrix4fv(worldMatrixLocation, 1, GL_FALSE, &pillarWorldMatrix[0][0]);
         glDrawArrays(GL_TRIANGLES, 0, 36);
+        
+        for (int i=0; i<20; ++i)
+        {
+            for (int j=0; j<20; ++j)
+            {
+                pillarWorldMatrix = translate(mat4(1.0f), vec3(- 100.0f + i * 10.0f, 5.0f, -100.0f + j * 10.0f)) * scale(mat4(1.0f), vec3(1.0f, 10.0f, 1.0f));
+                glUniformMatrix4fv(worldMatrixLocation, 1, GL_FALSE, &pillarWorldMatrix[0][0]);
+                glDrawArrays(GL_TRIANGLES, 0, 36);
+                
+                pillarWorldMatrix = translate(mat4(1.0f), vec3(- 100.0f + i * 10.0f, 0.55f, -100.0f + j * 10.0f)) * rotate(mat4(1.0f), radians(180.0f), vec3(0.0f, 1.0f, 0.0f)) * scale(mat4(1.0f), vec3(1.1f, 1.1f, 1.1f));
+                glUniformMatrix4fv(worldMatrixLocation, 1, GL_FALSE, &pillarWorldMatrix[0][0]);
+                glDrawArrays(GL_TRIANGLES, 0, 36);
+            }
+        }
+        
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+
+        
         
         // End Frame
         glfwSwapBuffers(window);
         glfwPollEvents();
-
+        
         // Handle inputs
         if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
             glfwSetWindowShouldClose(window, true);
+        
+        if (glfwGetKey(window, GLFW_KEY_1) == GLFW_PRESS) // move camera down
+        {
+            cameraFirstPerson = true;
+        }
 
-        // Fast cam parameters:
+        if (glfwGetKey(window, GLFW_KEY_2) == GLFW_PRESS) // move camera down
+        {
+            cameraFirstPerson = false;
+        }
+
+        
+        // This was solution for Lab02 - Moving camera exercise
+        // We'll change this to be a first or third person camera
         bool fastCam = glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS || glfwGetKey(window, GLFW_KEY_RIGHT_SHIFT) == GLFW_PRESS;
         float currentCameraSpeed = (fastCam) ? cameraFastSpeed : cameraSpeed;
         
-        // Calculate mouse motion dx dy
-        double mousePosX, mousePosY;
-        glfwGetCursorPos(window,&mousePosX, &mousePosY);
         
+        // @TODO 4 - Calculate mouse motion dx and dy
+        //         - Update camera horizontal and vertical angle
+        //...
+        double mousePosX, mousePosY;
+        glfwGetCursorPos(window, &mousePosX, &mousePosY);
+
         double dx = mousePosX - lastMousePosX;
         double dy = mousePosY - lastMousePosY;
+
         lastMousePosX = mousePosX;
         lastMousePosY = mousePosY;
 
-        // Convert to spherical coordinates
-        const float cameraAngularSpeed = 3.0f;
-        cameraHorizontalAngle-= dx * cameraAngularSpeed *dt;
-        cameraVerticalAngle  -= dy * cameraAngularSpeed *dt;
+        const float cameraAngularSpeed = 6.0f;
+        cameraHorizontalAngle -= dx * cameraAngularSpeed * dt;
+        cameraVerticalAngle -= dy * cameraAngularSpeed * dt;
 
-        // Clamp vertical angle to [-85, 85] degrees
         cameraVerticalAngle = std::max(-85.0f, std::min(85.0f, cameraVerticalAngle));
-        if(cameraHorizontalAngle> 360){
+        if(cameraHorizontalAngle > 360)
             cameraHorizontalAngle -= 360;
-        }
-        else if (cameraHorizontalAngle < -360){
-            cameraHorizontalAngle =+360;
-        }
+        else if (cameraHorizontalAngle < - 360)
+            cameraHorizontalAngle += 360;
+
         float theta = radians(cameraHorizontalAngle);
         float phi = radians(cameraVerticalAngle);
 
-        cameraLookAt = vec3(cosf(phi)*cosf(theta), sinf(phi), -cos(phi)*sinf(theta));
+        cameraLookAt = vec3(cosf(phi)*cosf(theta), sinf(phi), -cosf(phi)*sinf(theta));
         vec3 cameraSideVector = glm::cross(cameraLookAt, vec3(0.0f, 1.0f, 0.0f));
-        
-        glm::normalize(cameraSideVector);
 
-        // Inputs for moving camera around:
-         if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) // move camera to the left
+        glm::normalize(cameraSideVector);
+        
+        // @TODO 5 = use camera lookat and side vectors to update positions with ASDW
+        // adjust code below
+        if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) // move camera to the left
         {
-            cameraPosition -= cameraSideVector * currentCameraSpeed * dt;
+            cameraPosition.x -= currentCameraSpeed * dt * currentCameraSpeed;
         }
         
         if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) // move camera to the right
         {
-            cameraPosition += cameraSideVector *currentCameraSpeed * dt;
+            cameraPosition.x += currentCameraSpeed * dt * currentCameraSpeed;
         }
         
         if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) // move camera up
         {
-            cameraPosition -= cameraLookAt *currentCameraSpeed * dt;
+            cameraPosition.y -= currentCameraSpeed * dt * currentCameraSpeed;
         }
         
         if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) // move camera down
         {
-            cameraPosition += cameraLookAt *currentCameraSpeed * dt;
+            cameraPosition.y += currentCameraSpeed * dt * currentCameraSpeed;
         }
+      
+        // TODO 6
+        // Set the view matrix for first and third person cameras
+        // - In first person, camera lookat is set like below
+        // - In third person, camera position is on a sphere looking towards center
+        mat4 viewMatrix = mat4(1.0);
+        
+        if(cameraFirstPerson){
+            viewMatrix = lookAt(cameraPosition, cameraPosition + cameraLookAt, cameraUp );
+        } else {
+            float radius = 5.0f;
+            glm::vec3 position = cameraPosition - radius * cameraLookAt;
+            viewMatrix = lookAt(position, position + cameraLookAt, cameraUp);
+        }
+
+        GLuint viewMatrixLocation = glGetUniformLocation(shaderProgram, "viewMatrix");
+        glUniformMatrix4fv(viewMatrixLocation, 1, GL_FALSE, &viewMatrix[0][0]);
+
+
     }
+
+    
     // Shutdown GLFW
     glfwTerminate();
     
